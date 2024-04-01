b96d1e9b3d6d38ae8d698dd3361591e839f31e0e
Alpaoca <preechak10123@gmail.com>
2024-04-01 16:42:07 +0700

    Finish Styling NavBar

---
+++ src/components/layout/Nav.tsx
@@ -0,0 +1,40 @@
+import NavButton, { NavButtonProps } from "../shared/buttons/NavButton";
+
+function Nav() {
+  let navButtons: NavButtonProps[] = [
+    { label: "Collections" },
+    { label: "Men" },
+    { label: "Women" },
+    { label: "About" },
+    { label: "Contact" },
+  ];
+
+  return (
+    <>
+      <div className="flex justify-between pb-8">
+        <div className="flex flex-row">
+          <img className="object-scale-down mr-4" src="images\icon-menu.svg" />
+          <img className="object-scale-down mr-8" src="images\logo.svg" />
+          <div className="flex flex-row">
+            {navButtons.map((item, index) => (
+              <NavButton key={index} label={item.label} />
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <img
+            className="inline object-scale-down mx-8"
+            src="images\icon-cart.svg"
+          />
+          <img
+            className="object-scale-down size-12"
+            src="images\image-avatar.png"
+          />
+        </div>
+      </div>
+      <hr className="border-grayish-blue" />
+    </>
+  );
+}
+
+export default Nav;
